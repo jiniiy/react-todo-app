5a749d3f6f698438c71d21a03da20207aecfef82
jinmo <jinmo_kim@skal.suntory.co.jp>
2022-07-30 20:07:38 +0900

    todoList-KeypropsAdd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+export default class App extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button>X</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
