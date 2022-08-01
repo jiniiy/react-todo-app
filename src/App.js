ba84fd163a6352cef1478a1819f8948d5b781ac0
jinmo <jinmo_kim@skal.suntory.co.jp>
2022-08-01 20:33:52 +0900

    input formList submit function

--- src/App.js
+++ src/App.js
@@ -30,10 +30,9 @@ export default class App extends Component {
       title:"차트분석하기",
       completed:false,
     },
-  ]
-
-
-  }
+  ],
+  value: ""
+};
 
   btnStyle = {
     color: "#fff",
@@ -59,6 +58,29 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData })
   }
 
+  handleChange = (e) => {
+    console.log('e',e.target.value);
+    this.setState({ value: e.target.value});
+  }
+
+  handSubmit = (e) => {
+    //form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌.
+    e.preventDefault();
+
+    //새로운 할 일 데이터
+    let newTodo = {
+      id:Date.now(),
+      title:this.state.value,
+      completed: false,
+
+    };
+
+    //본래 존재하던 할 일에 새로운 할 일 더해주기 업데이트 개념.
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    
+
+  }
+
   render(){
     return(
       <div className="container">
@@ -71,10 +93,33 @@ export default class App extends Component {
           <div style={this.getStyle()} key={data.id} >
             <input type="checkbox" defaultChecked={false} />
               {data.title}
-            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>X</button>
+            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}
+            >
+              X
+            </button>
           </div>
-        
         ))}
+
+            <form style={{display:'flex'}} onSubmit={this.handSubmit}>
+              <input 
+                type="text" 
+                name="value" 
+                style={{ flex: '10', padding: '5px'}}
+                placeholder= "해야 할 일을 입력하세요."
+                value= {this.state.value}     //state관리할 부분
+                onChange={this.handleChange}
+
+              />
+              <input
+                type="submit"
+                value="입력"
+                className="btn"
+                style={{flex:'1'}}
+              />
+
+
+            </form>
+
         </div>
       </div>
     )
