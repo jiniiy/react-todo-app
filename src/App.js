9365316985dd6e8702180ff03701a8e5edb1a3ea
jinmo <jinmo_kim@skal.suntory.co.jp>
2022-08-09 22:47:27 +0900

    class component -> function component change by using reactHooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import "./App.css";
-export default class App extends Component {
-  state = {
-    //dummy data
-  todoData : [],
-  value: ""
-};
 
-  btnStyle = {
+export default function App() {
+  const[todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const btnStyle = {
     color: "#fff",
     border: "none",
     padding: "5px 9px",
@@ -16,7 +14,7 @@ export default class App extends Component {
     float:"right"
   }
 
-  getStyle = (completed) =>{
+  const getStyle = (completed) =>{
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
@@ -24,48 +22,42 @@ export default class App extends Component {
     }
   }
   
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter(data => data.id !== id)
-    console.log('newTodoData', newTodoData)
-    
-    this.setState({ todoData: newTodoData })
-  }
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter(data => data.id !== id)
+    setTodoData(newTodoData)
+  };
 
-  handleChange = (e) => {
-    console.log('e',e.target.value);
-    this.setState({ value: e.target.value});
-  }
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-  handSubmit = (e) => {
+  const handSubmit = (e) => {
     //form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌.
     e.preventDefault();
 
     //새로운 할 일 데이터
     let newTodo = {
       id:Date.now(),
-      title:this.state.value,
+      title:value,
       completed: false,
-
     };
 
     //본래 존재하던 할 일에 새로운 할 일 더해주기 업데이트 개념.
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
-    
-  }
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
+  };
 
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map(data => {
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map(data => {
       if(data.id === id) {
         data.completed = !data.completed;
       }
       return data;
-    })
+    });
+    setTodoData(newTodoData);
+  };
 
-    this.setState({ todoData:newTodoData});
-
-  }
-
-  render(){
+ 
     return(
       <div className="container">
         <div className="todoBlock">
@@ -73,25 +65,30 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-      {this.state.todoData.map((data) => (
-          <div style={this.getStyle(data.completed)} key={data.id} >
-            <input type="checkbox" defaultChecked={false} onChange={() => this.handleCompleteChange(data.id)} />
+      {todoData.map((data) => (
+          <div style={getStyle(data.completed)} key={data.id} >
+            <input 
+            type="checkbox" 
+            defaultChecked={false} 
+            onChange={() => handleCompleteChange(data.id)} />
               {data.title}
-            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}
+            <button 
+            style={btnStyle}
+             onClick={() => handleClick(data.id)}
             >
               X
             </button>
           </div>
         ))}
 
-            <form style={{display:'flex'}} onSubmit={this.handSubmit}>
+            <form style={{display:'flex'}} onSubmit={handSubmit}>
               <input 
                 type="text" 
                 name="value" 
                 style={{ flex: '10', padding: '5px'}}
                 placeholder= "해야 할 일을 입력하세요."
-                value= {this.state.value}     //state관리할 부분
-                onChange={this.handleChange}
+                value= {value}     //state관리할 부분
+                onChange={handleChange}
 
               />
               <input
@@ -100,11 +97,8 @@ export default class App extends Component {
                 className="btn"
                 style={{flex:'1'}}
               />
-
             </form>
-
         </div>
       </div>
-    )
-  }
+    );
 }
